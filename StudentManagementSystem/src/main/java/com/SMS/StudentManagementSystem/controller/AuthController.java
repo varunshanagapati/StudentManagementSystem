12aeb3b3c25bfc6566b64758e7787b61d870ff96
Varun <varunshanagapati1@gmail.com>
2025-05-10 10:34:52 +0530

    Commit from VsCode

--- StudentManagementSystem/src/main/java/com/SMS/StudentManagementSystem/controller/AuthController.java
+++ StudentManagementSystem/src/main/java/com/SMS/StudentManagementSystem/controller/AuthController.java
@@ -19,69 +19,69 @@ public class AuthController {
 	@Autowired
 	private AdminService adminService;
 
-    
-	
 	@GetMapping("/")
 	public String showIndexPage() {
 		return "index";
 	}
-	
+
 	@GetMapping("/adminlogin")
 	public String showLoginPage() {
 		return "adminloginpage";
 	}
-	
+
 	@GetMapping("/adminsignup")
 	public String showSignupPage() {
 		return "adminsignuppage";
 	}
-	
+
 	@PostMapping("/adminlogin")
-	public String adminLogin(@RequestParam("adminLoginId") int loginId,@RequestParam("adminLoginPassword") String loginPassword,Model model, HttpSession httpSession) {
-		
+	public String adminLogin(@RequestParam("adminLoginId") int loginId,
+			@RequestParam("adminLoginPassword") String loginPassword, Model model, HttpSession httpSession) {
+
 		try {
-			AdminLoginEntity adminLoginDto=new AdminLoginEntity();
+			AdminLoginEntity adminLoginDto = new AdminLoginEntity();
 			adminLoginDto.setAdminLoginId(loginId);
 			adminLoginDto.setAdminLoginPassword(loginPassword);
-			boolean isValid=adminService.verifyAdminLogin(adminLoginDto);
-			if(isValid) {
-				
-				String uname=adminService.getAdminName(loginId);
+			boolean isValid = adminService.verifyAdminLogin(adminLoginDto);
+			if (isValid) {
+
+				String uname = adminService.getAdminName(loginId);
 				httpSession.setAttribute("LoggedInUser", uname);
-//				System.out.println(uname);
-				model.addAttribute("user",uname);
+				// System.out.println(uname);
+				model.addAttribute("user", uname);
+				System.out.println("Checking session");
 				return "redirect:/dashboard";
-			}else {
+			} else {
 				model.addAttribute("error", "Invalid username or password");
-		        return "redirect:/";
+				return "redirect:/";
 
 			}
-		}catch (Exception e) {
-			//server related error mention her
+		} catch (Exception e) {
+			// server related error mention her
 			System.out.println(e);
 			return "redirect:/adminlogin";
 		}
 	}
-	
+
 	@PostMapping("/adminsignup")
 	public String adminsignup(@RequestParam("adminSignupName") String signupName,
 			@RequestParam("adminsignupId") int signupId,
 			@RequestParam("adminSignupEmail") String signupEmail,
 			@RequestParam("adminSignupPassword") String signupPassword,
 			Model model, HttpSession httpSession) {
-		
+
 		try {
 			AdminEntity adminDto = new AdminEntity();
 			adminDto.setAdminId(signupId);
 			adminDto.setAdminName(signupName);
 			adminDto.setAdminEmail(signupEmail);
 			adminDto.setAdminPassword(signupPassword);
-//			System.out.println(adminDto);
-			String resp=adminService.addAdmin(adminDto);
-//			System.out.println(resp);
+			// System.out.println(adminDto);
+			String resp = adminService.addAdmin(adminDto);
+			// System.out.println(resp);
 			return "redirect:/adminlogin";
-		}catch (Exception e) {
-			//server related error mention her
+		} catch (Exception e) {
+			// server related error mention her
 			System.out.println(e);
 			return "redirect:/";
 		}
